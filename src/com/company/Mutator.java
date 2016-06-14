2fb887efc571bef6d97d140c4879f8216c77ec2e
agnie <agnieszka.pula@gmail.com>
2016-06-14 20:56:48 +0200

    Added mutator

---
+++ src/com/company/Mutator.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by agnie on 6/14/2016.
+ */
+public class Mutator {
+
+    public void mutate(ArrayList<Specimen> population, double mutationProbability){
+        //todo
+    }
+
+}
