1720c416f2daaf1ff12d7644719d373b873ab42b
agnie <agnieszka.pula@gmail.com>
2016-06-19 18:43:45 +0200

    Small update to evaluator

--- src/com/company/Evaluator.java
+++ src/com/company/Evaluator.java
@@ -15,13 +15,16 @@ public class Evaluator {
             int errorCount = 0;
             for (int i = 0; i < specimen.getSize(); i++) {
                 ArrayList<Edge> edges = specimen.getEdges(i);
+//                System.out.println("Node edges: " + edges);
                 if (edges != null) {
                     for (Edge edge : edges) {
+                    if (edge.getBeginNode()!=edge.getEndNode()){
                         int nodeColour = specimen.getColour(i);
                         int neighbourColour = specimen.getColour(specimen.indexOf(specimen.findNode(edge.getEndNode())));
                         if (Math.abs(nodeColour - neighbourColour) < edge.getWeight()) {
                             errorCount = errorCount + (edge.getWeight() - Math.abs(nodeColour - neighbourColour));
                         }
+                    }
                     }
                 }
             }
